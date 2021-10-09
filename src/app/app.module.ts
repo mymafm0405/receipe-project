ca6eddac302f6d39968a4d176377926eac0b3ddf
Mahmoud <mymafm0405@gmail.com>
2021-10-09 11:00:02 +0300

    Finish adding the base of the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecepiesComponent } from './recepies/recepies.component';
 import { RecepiesListComponent } from './recepies/recepies-list/recepies-list.component';
 import { RecepieItemComponent } from './recepies/recepies-list/recepie-item/recepie-item.component';
 import { RecepieDetailsComponent } from './recepies/recepie-details/recepie-details.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecepiesComponent,
     RecepiesListComponent,
     RecepieItemComponent,
     RecepieDetailsComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    HeaderComponent,
   ],
   imports: [BrowserModule],
   providers: [],
